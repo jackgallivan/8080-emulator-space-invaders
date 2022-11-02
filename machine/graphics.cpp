a645b25044ba5c163d142c98b47f305fa609614e
nelsydaperez <pereznel@oregonstate.edu>
2022-11-01 23:50:03 -0400

    added methods required to run machine and display game

--- machine/graphics.cpp
+++ machine/graphics.cpp
@@ -1,42 +1,29 @@
-void draw_video_ram() {
-    uint32_t *pix = surf->pixels;
+#include "machine.hpp"
 
-    int i = 0x2400;  // Start of Video RAM
-    for (int col = 0; col < SCREEN_WIDTH; col ++) {
-        for (int row = SCREEN_HEIGHT; row > 0; row -= 8) {
-            for (int j = 0; j < 8; j++) {
-                int idx = (row - j) * SCREEN_WIDTH + col;
-
-                if (ram->mem[i] & 1 << j) {
-                    pix[idx] = 0xFFFFFF;
-                } else {
-                    pix[idx] = 0x000000;
+namespace space_invaders
+{
+    void Machine::update_screen()
+    {
+        uint32_t *pix {(uint32_t *) disp_->pixels};
+        int i {0x2400}; // start of video ram
+        for (int col {0}; col < SCREEN_WIDTH; ++col)
+        {
+            for (int row {SCREEN_HEIGHT}; row > 0; row -= 8)
+            {
+                for (int j {0}; j < 8; ++j)
+                {	
+                    int idx = (row - j) * SCREEN_WIDTH + col;
+                    if (memory_[i] & 1 << j)
+                        pix[idx] = 0xFFFFFF;
+                    else
+                        pix[idx] = 0x000000;
                 }
+                ++i;
             }
-
-            i++;
         }
+        SDL_Surface *winsurf = SDL_GetWindowSurface(window_);
+        SDL_BlitScaled(disp_, NULL, winsurf, NULL);
+        if (SDL_UpdateWindowSurface(window_))
+            std::cerr << SDL_GetError();
     }
-
-    if (resizef) {
-        winsurf = SDL_GetWindowSurface(win);
-    }
-
-    SDL_BlitScaled(surf, NULL, winsurf, NULL);
-
-    // Update window
-    if (SDL_UpdateWindowSurface(win)) {
-        puts(SDL_GetError());
-    }
-}
-
-int HandleResize(void *userdata, SDL_Event *ev) {
-    if (ev->type == SDL_WINDOWEVENT) {
-        if (ev->window.event == SDL_WINDOWEVENT_RESIZED) {
-            resizef = 1;
-        }
-    }
-
-    return 0;  // Ignored
-}
 }
--- machine/machine.hpp
+++ machine/machine.hpp
@@ -1,4 +1,31 @@
 #include <SDL2/SDL.h>
 
+extern "C" {
+    #include "../emulator/emulator.h"
+}
+
 #define SCREEN_HEIGHT 256
 #define SCREEN_WIDTH 224
+
+namespace space_invaders
+{
+    class Machine
+    {
+        public:
+        Machine();
+        void run();
+	    void load_program();
+        void update_screen();
+        
+        private:
+        State_8080 cpu_ {init_8080()};
+        
+        bool done_ {false};
+        std::array<std::array<std::array<uint8_t, 3>, SCREEN_WIDTH>, SCREEN_HEIGHT> screen_buf_ {};
+        SDL_Window *window_;
+        SDL_Surface *disp_;
+        
+        void execute_cpu(long cyc);        
+    };
+
+}
