a318716683ad710fa5ff3df0536b14810a0a6e28
Nelsyda Perez <49159549+nelsydaperez@users.noreply.github.com>
2022-11-03 06:41:26 -0400

    Update main.cpp
    Added code to run machine cabinet code.

--- main.cpp
+++ main.cpp
@@ -3,3 +3,17 @@ extern "C"
 #include "emulator/emulator.h"
 }
 #include "machine/machine.hpp"
+
+int main(int argc, char *argv[])
+{
+	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) < 0)
+	{
+		fprintf(stderr, "SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+		throw;
+	}
+	space_invaders::Machine cabinet {};
+	cabinet.load_program();
+	cabinet.run();
+	SDL_Quit();
+	return 0;
+}
