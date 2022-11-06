743bd15fb91e47c6f35f36628bcb54e44d200592
jackgallivan <jackmgallivan@gmail.com>
2022-11-06 15:49:16 -0800

    fix struct member reference

--- machine/machine.hpp
+++ machine/machine.hpp
@@ -18,15 +18,15 @@ namespace space_invaders
         void run();
 	    void load_program();
         void update_screen();
-        
+
         private:
         State_8080 *cpu_ {init_8080()};
-        
+
         bool done_ {false};
         std::array<std::array<std::array<uint8_t, 3>, SCREEN_WIDTH>, SCREEN_HEIGHT> screen_buf_ {};
         SDL_Window *window_;
         SDL_Surface *disp_;
-        
+
         void execute_cpu();    // TODO: add cycles variable as a long
     };
 
