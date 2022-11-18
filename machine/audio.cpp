a5554cc88e4ed132143a54871da32eba52721d27
jackgallivan <jackmgallivan@gmail.com>
2022-11-17 22:47:49 -0800

    audio.cpp improvements

--- machine/audio.cpp
+++ machine/audio.cpp
@@ -1,28 +1,32 @@
 #include "machine.hpp"
 
 Wav::Wav(const std::string &s)
-	: spec_{}, buf_{nullptr}, len_{0}
 {
 	std::string path{s + ".wav"};
-	if (SDL_LoadWAV(path.c_str(), &spec_, &buf_, &len_) == NULL)
+	if (!SDL_LoadWAV(path.c_str(), &spec_, &buf_, &len_))
 		throw std::runtime_error(std::string("SDL_LoadWAV failed! SDL Error: ") + SDL_GetError());
-	SDL_AudioSpec obtained;     // format obtained
-	dev_ = SDL_OpenAudioDevice(nullptr, 0, &spec_, &obtained, 0);
-	if (dev_ == 0)
+	dev_ = SDL_OpenAudioDevice(nullptr, 0, &spec_, nullptr, 0);
+	if (!dev_)
 		throw std::runtime_error(std::string("SDL_OpenAudioDevice failed! SDL Error: ") + SDL_GetError());
+	SDL_PauseAudioDevice(dev_, 0);
 }
 
+/**
+ * Close the object's audio device and free its audio buffer
+ */
 Wav::~Wav()
 {
 	SDL_CloseAudioDevice(dev_);
 	SDL_FreeWAV(buf_);
 }
 
+/**
+ * Queue audio to play once
+ */
 void Wav::play()
 {
-	if (SDL_QueueAudio(dev_, buf_, len_) < 0)
+	if (SDL_QueueAudio(dev_, buf_, len_))
 		throw std::runtime_error(std::string("SDL_QueueAudio failed! SDL Error: ") + SDL_GetError());
-	SDL_PauseAudioDevice(dev_, 0);
 }
 
 void Wav::loop()
@@ -32,6 +36,9 @@ void Wav::loop()
 namespace space_invaders
 {
 
+/**
+ * Plays the sounds corresponding to sound port values
+ */
 void Machine::play_sound()
 {
 	if (sound_port_3 != last_sound_3)     // bit changed
@@ -86,6 +93,7 @@ void Machine::play_sound()
 		// UFO hit
 		if ((sound_port_5 & 0x10) && !(last_sound_5 & 0x10))
 			sounds_[8].play();
+
 		last_sound_5 = sound_port_5;
 	}
 }
